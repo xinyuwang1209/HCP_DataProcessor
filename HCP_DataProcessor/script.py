8f0146064d5c141e059401d556a2e02b1b2eb444
Xinyu Wang <xinyu.wang@uconn.edu>
2019-05-23 13:36:34 -0400

    add pca_filter

--- HCP_DataProcessor/script.py
+++ HCP_DataProcessor/script.py
@@ -3,7 +3,6 @@ import pandas as pd
 # from Prediction_Utils.Age_Preddiction.Use_Lasso import run_lasso_cv
 # from Prediction_Utils.Age_Preddiction.Use_SVR   import run_svm_cv
 # from Prediction_Utils.Age_Preddiction.Use_RF    import run_rf_cv
-import HCP_DataProcessor.Data_Process as HCPDPP
 
 #
 #
@@ -16,12 +15,14 @@ import HCP_DataProcessor.Data_Process as HCPDPP
 # feat = run_svm_cv(X,y,kernel='poly',C=0.5,cv=5,degree=10,coef0=100)
 # feat = run_svm_cv(X,y,kernel='poly',C=0.5,cv=5,degree=1)
 
-
-
-def get_data(path=None,use_4_features=False):
-    data = HCPDPP.correlation_matrix_collector()
-    data = HCPDPP.update_addiction(data)
-    data = HCPDPP.filter_has_genotype(data)
-    data = HCPDPP.filter_quality_test(data)
-    X,y = HCPDPP.get_X_y(data,use_4_features=use_4_features)
-    return X,y
+def get_data(path=None,use_4_features=False,pca_filter=0):
+    data = .DataProcess.correlation_matrix_collector()
+    data = .DataProcess.update_addiction(data)
+    data = .DataProcess.filter_has_genotype(data)
+    data = .DataProcess.filter_quality_test(data)
+    X,y = .DataProcess.get_X_y(data,use_4_features=use_4_features)
+    if pca_filter > 0:
+        X,a = .DataProcess.pca_filter(X,pca_filter)
+        return X,y,a
+    else:
+        return X,y
